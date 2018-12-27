2cca78dd8cd0a68d63d55a5bdef6018040957845
slammad <saidubello212@gmail.com>
2018-12-27 12:49:02 +0100

    commit

--- events.php
+++ events.php
@@ -11,21 +11,23 @@
 
 
 <?php include 'partials/top.inc.php' ?>
+<br><br><br>
 <div class="error">
-        <div class="error-code m-b-10 m-t-20">404 <i class="fa fa-warning"></i></div>
-        <h3 class="font-bold">We couldn't find the page..</h3>
+        <div class="error-code m-b-10 m-t-20">503 <i class="fa fa-warning"></i></div>
+        <h3 class="font-bold">Oops Page Under Construction.</h3>
 
         <div class="error-desc">
-            Sorry, but the page you are looking for was either not found or does not exist. <br/>
-            Try refreshing the page or click the button below to go back to the Homepage.
+            Sorry, but the page you are looking for is under construction. <br/>
+            Try checking back later thank you.
             <div>
-                <a class=" login-detail-panel-button btn" href="http://www.vmware.com/">
+                <a class=" login-detail-panel-button btn" href="http://www.beginnersprideacademy.com/">
                         <i class="fa fa-arrow-left"></i>
                         Go back to Homepage                        
                     </a>
             </div>
         </div>
     </div>
+    <br><br><br>
 <?php
     include 'partials/script.php';
     include 'includes/footer.inc.php';
