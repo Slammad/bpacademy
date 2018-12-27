deecce7513a6fd785b804766af61292349813d48
slammad <saidubello212@gmail.com>
2018-12-27 12:20:47 +0100

    pushed

--- admin/dashboard.php
+++ admin/dashboard.php
@@ -48,7 +48,7 @@ include 'partials/adminhead.inc.php';
     
                                 <a class="dropdown-item" href="javascript:void(0)"><i class="ti-settings m-r-5 m-l-5"></i> Account Setting</a>
                                 <div class="dropdown-divider"></div>
-                                <a class="dropdown-item" href="javascript:void(0)"><i class="fa fa-power-off m-r-5 m-l-5"></i> Logout</a>
+                                <a class="dropdown-item" href="logout.php"><i class="fa fa-power-off m-r-5 m-l-5"></i> Logout</a>
   
                             </div>
                         </li>
