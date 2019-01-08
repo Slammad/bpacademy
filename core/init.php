052565686e3f077107ea197ad576528f2ec3f0eb
slammad <saidubello212@gmail.com>
2019-01-08 19:32:37 +0000

    changes

--- core/init.php
+++ core/init.php
@@ -1,8 +1,8 @@
 <?php
     $localhost = "localhost";
-    $user = "slammad";
-    $password = "Slammad42";
-    $db = "bpacademy";
+    $user = "beginnersprideac";
+    $password = "7RHZw86ITxliiB2";
+    $db = "beginnersprideacademy";
 
 
     $conn = mysqli_connect($localhost,$user,$password,$db) or die('failed to connect to db');
